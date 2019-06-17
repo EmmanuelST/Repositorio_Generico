9bc778234a2b354b3b9bb79919c4c253c559e281
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-17 19:40:17 -0400

    Agregando el resto

---
+++ RepositorioGenerico/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using RepositorioGenerico.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositorioGenerico.DAL
+{
+    class Contexto : DbContext
+    {
+
+        public DbSet<Ciudades>Ciudad { get; set; }
+        public Contexto() : base("Constr")
+        {
+
+        }
+    }
+}
---
+++ RepositorioGenerico/Entidades/Ciudades.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositorioGenerico.Entidades
+{
+    class Ciudades
+    {
+        [Key]
+        public int CiudadId { get; set; }
+        public string Descripcion { get; set; }
+
+        public Ciudades()
+        {
+            CiudadId = 0;
+            Descripcion = string.Empty;
+        }
+
+    }
+}
